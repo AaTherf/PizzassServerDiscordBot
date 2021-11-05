d26204721355f444fe9db18d401363c45ba51486
AaTherf <atharv.dewangan@rediffmail.com>
2021-11-05 12:25:47 +0530

    Added MongoDB

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 const config = require('./config.json')
+const mongo = require('./mongo')
 const loadCommands = require('./commands/load-commands')
+const levels = require('./levels')
 
 const welcome = require('./welcome')
+const commandBase = require('./commands/command-base')
 
 client.on('ready', async () => {
   console.log('The client is ready!')
 
   loadCommands(client)
+  levels(client)
   
   const { prefix } = config
 
@@ -17,6 +21,14 @@ client.on('ready', async () => {
     activity: {
       name: `"${prefix}help" for help!`,
     },
+  })
+
+  await mongo().then(mongoose => {
+    try{
+      console.log('Connected to Mongo!')
+    } finally {
+      mongoose.connection.close()
+    }
   })
 
 })
